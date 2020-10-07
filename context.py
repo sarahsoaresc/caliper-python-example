92469dd848ae04514ce4b89dd0b91f2e1a53d856
Sarah Soares <sarahsoaresc@users.noreply.github.com>
2020-10-06 23:41:40 -0500

    update

--- context.py
+++ context.py
@@ -1,6 +1,14 @@
 import datetime
 
+# Sensor configuration
 import caliper
+
+sensor_config = caliper.HttpOptions(
+    host='https://test_endpoint_url',
+    auth_scheme='Sarah',
+    api_key='test_api_key'
+)
+
 from caliper import entities
 from caliper.constants import CALIPER_ROLES, CALIPER_STATUS
 
@@ -10,15 +18,15 @@ COURSE_IRI = BASE_URI+"/course/2017/ssed514"
 # User
 example_user = entities.Person(
     id=BASE_URI+"/user/193828",
-    name="Test User"
+    name="Teste"
 )
 
 # Course
 example_course = entities.CourseSection(
     id=COURSE_IRI,
-    courseNumber="SSED514",
-    name="Economy and Society",
-    category="lecture",
+    courseNumber="Teste",
+    name="informática",
+    category="avaliação",
 )
 
 # SoftwareApplication
@@ -36,14 +44,14 @@ homepage = entities.DigitalResource(
 
 reading_material = entities.WebPage(
     id=COURSE_IRI+"/document/11",
-    name="Welfare Economics",
-    description="Introduction of welfare economics"
+    name="informática",
+    description="introdução a informática básica"
 )
 
 # Assessment
 assessment = entities.Assessment(
     id=COURSE_IRI+"/assessment/1",
-    name="Questions for Economics",
+    name="introdução a informática básica",
     isPartOf=example_course,
     maxAttempts=3,
     maxSubmits=2,
@@ -68,7 +76,7 @@ example_session = entities.Session(
 # Sensor configuration
 sensor_config = caliper.HttpOptions(
     host='https://caliper.imsglobal.org/caliper/849fab7f-72a4-4398-a4e5-e0eaec97170b/message',
-    auth_scheme='Bearer',
+    auth_scheme='Sarah',
     api_key='849fab7f-72a4-4398-a4e5-e0eaec97170b'
 )
 
